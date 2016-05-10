4d59b8f046b7c9b658bf29cc1c48751c73ae9916
Simon Tseng <nomis5888@gmail.com>
2016-05-10 16:49:07 +0800

    Initial Commit

---
+++ ColorAdapativeStatusBarExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorAdapativeStatusBarExample
+//
+//  Created by Simon on 2016/5/10.
+//  Copyright © 2016年 Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
